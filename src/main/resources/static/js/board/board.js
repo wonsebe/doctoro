c5db30e65cbfd3b5d72dc58bb4e0cdbf0f33ee88
wonsebe <wonsb03129@gmail.com>
2024-09-10 17:55:09 +0900

    원세빈 5시54분 커밋 게시판 전체출력 완료

--- src/main/resources/static/js/board/board.js
+++ src/main/resources/static/js/board/board.js
@@ -2,28 +2,25 @@ console.log('board.js');
 
 print();
 function print(){
-    let printBox= document.querySelector('#printBox'); console.log(printBox)
-    let html='' 
+
     console.log('html');
     $.ajax({
-        url: 'get',
-        method:'/board/print',
-        success: response =>{
-            console.log(response);
-            html+=`<tr> <th> ${response.bno}  </th> 
-            <th><a href="/board/detail?bno=${response.bno}">${response.btitle}</a>  </th> 
-            <th> ${response.bcontent}  </th> <th> ${response.bdate}  </th> 
-            <th> ${response.categoryno}  </th>  </tr>`
-
-            console.log(response);
+        async:false,
+        url: "/board/print",
+        method:"get",
+        success: r =>{
+            console.log(r);
+            let printBox= document.querySelector('#printBox');
+            let html=''
+            r.forEach(내용 =>{
+                
+            html+=` 
+                        <tr> <th> ${내용.bno} </th> 
+                        <th> <a href="/board/detail.html"></a>${내용.btitle}</th></tr><br/>
+             `
+        })
             printBox.innerHTML=html;
-            
+            console.log(printBox)
         }
-
-
-    });
-   
-
-
-
+    })
 }
--- src/main/resources/static/js/board/write.js
+++ src/main/resources/static/js/board/write.js
@@ -2,11 +2,32 @@ console.log('write.js');
 
 
 function boardWrite(){
-    let btitle=document.querySelector('#btitle').value
-    let bcontent=document.querySelector('#bcontent').value
+    let btitle=document.querySelector('#btitle').value;
+    let bcontent=document.querySelector('#bcontent').value;
+    // let categoryno=document.querySelector('#categoryno').value;
+    //조회수도???
 
-    let html='안녕??';
+    let html='';
     console.log(html)
+
+    $.ajax({
+        url:'post',
+        method:'write',
+        data:{ btitle: btitle,
+            bcontent:bcontent,
+            // categoryno :categoryno
+        },
+        success: (result) =>{
+            console.log(result);
+
+        },
+        error : (e) =>{
+            console.log(e);
+      }
+      
+    })
+
+
 
 
 }
