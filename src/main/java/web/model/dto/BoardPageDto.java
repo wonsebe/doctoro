c5a308be567fb17679b5de18dbef1a56cdd3896f
wonsebe <wonsb03129@gmail.com>
2024-09-12 18:19:37 +0900

    원세빈 6시 19분 집에가서 하자

--- src/main/java/web/model/dto/BoardPageDto.java
+++ src/main/java/web/model/dto/BoardPageDto.java
@@ -11,13 +11,13 @@ import java.util.List;
 @ToString
 @Builder
 public class BoardPageDto {
-    private int page; //현재 페이지 번호
-    private int totalPage; // 전체 페이지 수
-    private List<BoardDto> data; //조회된 게시물 정보 목록/리스트
-    private int startBtn; //페이지별 시작버튼 번호
-    private int endBtn; //페이지별 끝버튼 번호
-    private int categoryno; //현재 카테고리 번호
-    //+검색
-    private String searchKey;
-    private String searchKeyWord;
+    private int page; // 1. 현재 페이지 번호
+    // (지역/매개) 변수 와 필드 차이점 : 초기값의 차이
+    private int totalBoardSize; // 2. 전체 게시물수
+    private int totalPage; // 3. 전체 페이지수
+    private List<BoardDto> data; // 4. 조회된 게시물 정보 목록/리스트
+
+//    //+검색
+//    private String searchKey;
+//    private String searchKeyWord;
 }
