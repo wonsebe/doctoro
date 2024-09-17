92fc1a75b32ad77e29e26319c2c3a1c4f3bd53da
wonsebe <wonsb03129@gmail.com>
2024-09-17 23:43:49 +0900

    원세빈 게시판 카테고리별 출력 완료

--- src/main/java/web/service/CommetnplyService.java
+++ src/main/java/web/service/CommetnplyService.java
@@ -41,17 +41,17 @@ public class CommetnplyService {
     return commentplyDao.cPrint();
     }
 
-//    //댓글 수정
-//    public  boolean cUpdate(CommentDto commentDto){
-//    System.out.println("commentDto = " + commentDto);
-//    System.out.println("CommetnplyService.cUpdate");
-//    return commentplyDao.cUpdate(commentDto);
-//    }
-//
-//    //댓글 삭제
-//    public boolean cDelete(int cno){
-//    System.out.println("CommetnplyService.cDelete");
-//    System.out.println("cno = " + cno);
-//    return commentplyDao.cDelete(cno);
-//    }
+    //댓글 수정
+    public  boolean cUpdate(CommentDto commentDto){
+    System.out.println("commentDto = " + commentDto);
+    System.out.println("CommetnplyService.cUpdate");
+    return commentplyDao.cUpdate(commentDto);
+    }
+
+    //댓글 삭제
+    public boolean cDelete(int cno){
+    System.out.println("CommetnplyService.cDelete");
+    System.out.println("cno = " + cno);
+    return commentplyDao.cDelete(cno);
+    }
 }
