8a3c318d2e3b04177953970d0771c08366761d24
Lamool <gosu0522@naver.com>
2024-09-20 04:17:46 +0900

    240920 종족값 목록 - 총합 빼고 오름차순 내림차순 정렬 가능

--- src/main/resources/static/js/base-stats/base-stats.js
+++ src/main/resources/static/js/base-stats/base-stats.js
@@ -1,124 +1,34 @@
 console.log('base-stats.js')
 
-/*
-let num = 0;    // 번호 오름차순 내림차순 구분
+let sort = '';
+let word = '';
+let click = 0;      // 오름차순 내림차순 왔다갔다 할 수 있게 해주는 변수
 
-// 포켓몬 종족값 목록 출력 -> 아이디 (번호) 기준
-baseStatsAllPrint();
-function baseStatsAllPrint() {      console.log('baseStatsAllPrint()');
-    if (num == 0) {     // 번호 기준 오름차순
-        // Flask 서버에 통신
-        $.ajax({
-            async : false,
-            method : 'post',
-            url : "http://localhost:5000/base/stats/print",
-            contentType: 'application/json',
-            data: JSON.stringify({ data: 101010 }),
-            success : (result) => {     console.log(result);
-                let tbody = document.querySelector('tbody');
-                let html = ``;
-                result.forEach(포켓몬 => {
-                    // 종족값 총합 구하기
-                    let total = 포켓몬.체력
-                                + 포켓몬.공격
-                                + 포켓몬.방어
-                                + 포켓몬.특수공격
-                                + 포켓몬.특수방어
-                                + 포켓몬.스피드;
-                    // console.log(total);
-
-                    html += `
-                            <tr>
-                                <td>${포켓몬.아이디}</td>
-                                <td><img src="${포켓몬.이미지}" /></td>
-                                <td>${포켓몬.한글이름} (${포켓몬.영어이름})</td>
-                                <td>${포켓몬.타입}</td>
-                                <td>${포켓몬.체력}</td>
-                                <td>${포켓몬.공격}</td>
-                                <td>${포켓몬.방어}</td>
-                                <td>${포켓몬.특수공격}</td>
-                                <td>${포켓몬.특수방어}</td>
-                                <td>${포켓몬.스피드}</td>
-                                <td>${total}</td>
-                            </tr>
-                            `
-                })  // forEach end
-                tbody.innerHTML = html;
-            }   // success end
-        })  // ajax end
-
-        num = 1;
-    } else {    // 번호 기준 내림차순
-        // Flask 서버에 통신
-        $.ajax({
-            async : false,
-            method : 'get',
-            url : "http://localhost:5000/base/stats/print/pokeno",
-            success : (result) => {     console.log(result);
-                let tbody = document.querySelector('tbody');
-                let html = ``;
-                result.forEach(포켓몬 => {
-                    // 종족값 총합 구하기
-                    let total = 포켓몬.체력
-                                + 포켓몬.공격
-                                + 포켓몬.방어
-                                + 포켓몬.특수공격
-                                + 포켓몬.특수방어
-                                + 포켓몬.스피드;
-                    // console.log(total);
-
-                    html += `
-                            <tr>
-                                <td>${포켓몬.아이디}</td>
-                                <td><img src="${포켓몬.이미지}" /></td>
-                                <td>${포켓몬.한글이름} (${포켓몬.영어이름})</td>
-                                <td>${포켓몬.타입}</td>
-                                <td>${포켓몬.체력}</td>
-                                <td>${포켓몬.공격}</td>
-                                <td>${포켓몬.방어}</td>
-                                <td>${포켓몬.특수공격}</td>
-                                <td>${포켓몬.특수방어}</td>
-                                <td>${포켓몬.스피드}</td>
-                                <td>${total}</td>
-                            </tr>
-                            `
-                })  // forEach end
-                tbody.innerHTML = html;
-            }   // success end
-        })  // ajax end
-
-        num = 0;
-    }
-}   // baseStatsAllPrint() end
-*/
-
-
-
-
-
-
-
-
-
-// 포켓몬 종족값 목록 출력 -> 아이디 (번호) 기준
+// 포켓몬 종족값 목록 출력 - 기본 : 아이디(번호) 기준 정렬
 baseStatsAllPrint('아이디');
 function baseStatsAllPrint(name) {      console.log('baseStatsAllPrint()');
-    // let nameValue = '';
-    // if (name = '') {
-    //     nameValue = '아이디';
-    // }
-    // nameValue = name;
     console.log(name);
-    // console.log(nameValue);
+
+    // 같은 버튼을 연속해서 누른 경우 내림차순으로 정렬
+    if (word == name && click == 1) {
+        sort = '내림차순';
+        click = 0;
+    } else {
+        sort = '오름차순';
+        click = 1;
+    }
+    word = name;
+    
+
     // Flask 서버에 통신
     $.ajax({
         async : false,
         method : 'post',
-        url : "http://localhost:5000/base/stats/print/pokename",
+        url : "http://localhost:5000/base/stats/print/all",
         contentType: 'application/json',
         data: JSON.stringify({
             name : name,
-            sort : "내림차순"
+            sort : sort
         }),
         success : (result) => {     console.log(result);
             let tbody = document.querySelector('tbody');
