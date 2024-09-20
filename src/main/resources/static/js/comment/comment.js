23c8fe4b1cbf22e78a9e34032a05c9a3a5e6820c
wonsebe <wonsb03129@gmail.com>
2024-09-20 18:22:45 +0900

    원세빈 대댓글 mvc 삭제, css 가운데 정렬

--- src/main/resources/static/js/comment/comment.js
+++ src/main/resources/static/js/comment/comment.js
@@ -26,6 +26,72 @@ function comment(){ console.log('comment()');
     
 };
 
+console.log('cprint.js');
+
+
+commentPrint();
+function commentPrint(){
+    $.ajax({
+         async:false,
+         url:"/comment/cprint",
+         method:"get",
+         data:{bno:bno},
+         success: (response) => {
+            console.log(response);
+            let commentBox = document.querySelector('.commentBox');
+            let html = '';
+
+            if (response.length === 0) {
+                html = '<div>댓글이 없습니다.</div>';
+            } else {
+                response.forEach(comment => {
+                    html += `
+                            <div class="uid">${comment.id}</div>
+                           <span class="cContent">${comment.ccontent}</span>
+                           <span class="cdate">${comment.cdate}</span><br/><br/>
+                    `;
+                });
+            }
+
+            commentBox.innerHTML = html;
+        },
+        error: (e) => {
+            console.error("댓글을 불러오는 데 실패했습니다:", e);
+        }
+    })
+
+
+
+}
+
+function comment(){ console.log('comment()');
+
+    let ccontent=document.querySelector('.ccontent').value;
+    let info={ccontent:ccontent,
+                   bno:bno}
+    console.log(info)
+
+   $.ajax({
+        async : true,
+        method:'post',
+        url:"/comment/comentb",
+        data:info,
+        success:(r)=>{ console.log(r);
+             if(r){alert('댓글쓰기 성공')
+                commentPrint();
+                  document.querySelector('.ccontent').value=``
+             }else{alert('댓글쓰기 실패 :로그인 후 쓰기가 가능합니다.');}
+        },
+        error : (e) =>{
+             console.log(e);
+       }
+
+   })
+
+
+};
+
+
 
 
 
