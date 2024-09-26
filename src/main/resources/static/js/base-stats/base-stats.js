95d9ffd691eb2431a8765a53878e4c40deb2d439
Lamool <gosu0522@naver.com>
2024-09-27 04:25:03 +0900

    240927 종족값 목록 상위퍼센트 수정

--- src/main/resources/static/js/base-stats/base-stats.js
+++ src/main/resources/static/js/base-stats/base-stats.js
@@ -31,8 +31,25 @@ function baseStatsAllPrint(name) {      console.log('baseStatsAllPrint()');
             sort : sort
         }),
         success : (result) => {     console.log(result);
+            let thead = document.querySelector('thead > tr');
             let tbody = document.querySelector('tbody');
-            let html = ``;
+            let htmlTh = ``;
+            let htmlTb = ``;
+
+            htmlTh += `
+                    <th> <button type="button" onclick="baseStatsAllPrint('아이디')">번호</button> </th>
+                    <th> 이미지</th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('한글이름')">포켓몬</button> </th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('타입')">타입</button> </th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('체력')">체력</button> </th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('공격')">공격</button> </th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('방어')">방어</button> </th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('특수공격')">특수공격</button> </th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('특수방어')">특수방어</button> </th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('스피드')">스피드</button> </th>
+                    <th> <button type="button" onclick="baseStatsAllPrint('총합')">총합</button> </th>
+                    `
+
             result.forEach(포켓몬 => {
                 // 종족값 총합 구하기
                 let total = 포켓몬.체력
@@ -43,7 +60,7 @@ function baseStatsAllPrint(name) {      console.log('baseStatsAllPrint()');
                             + 포켓몬.스피드;
                 // console.log(total);
 
-                html += `
+                htmlTb += `
                         <tr>
                             <td>${포켓몬.아이디}</td>
                             <td><img src="${포켓몬.이미지}" /></td>
@@ -59,12 +76,14 @@ function baseStatsAllPrint(name) {      console.log('baseStatsAllPrint()');
                         </tr>
                         `
             })  // forEach end
-            tbody.innerHTML = html;
+            thead.innerHTML = htmlTh;
+            tbody.innerHTML = htmlTb;
         }   // success end
     })  // ajax end
 }   // baseStatsAllPrint() end
 
 // 특정 종족값에 대한 상위 퍼센트 계산
+// baseStatsPercentPrint('총합')
 function baseStatsPercentPrint(stats) {      console.log('baseStatsPercentPrint()');
     console.log(stats);
     $.ajax({
@@ -74,21 +93,73 @@ function baseStatsPercentPrint(stats) {      console.log('baseStatsPercentPrint(
         contentType: 'application/json',
         data : JSON.stringify({ stats : stats }),
         success : (result) => {     console.log(result);
+            let thead = document.querySelector('thead > tr');
             let tbody = document.querySelector('tbody');
-            let html = ``;
-            result.forEach(포켓몬 => {
-                html += `
-                        <tr>
-                            <td>${포켓몬.아이디}</td>
-                            <td><img src="${포켓몬.이미지}" /></td>
-                            <td>${포켓몬.한글이름} (${포켓몬.영어이름})</td>
-                            <td>${포켓몬.타입}</td>
-                            <td>${포켓몬[stats]}</td>
-                            <td>${포켓몬.상위퍼센트}</td>
-                        </tr>
+            let htmlTh = ``;
+            let htmlTb = ``;
+
+            if (stats == '총합') {     // stats가 총합인 경우 if문 실행
+                console.log('true');
+                htmlTh += `
+                        <th> <button type="button" onclick="baseStatsAllPrint('아이디')">번호</button> </th>
+                        <th> 이미지</th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('한글이름')">포켓몬</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('타입')">타입</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('체력')">체력</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('공격')">공격</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('방어')">방어</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('특수공격')">특수공격</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('특수방어')">특수방어</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('스피드')">스피드</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('총합')">총합</button> </th>
+                        <th> <button type="button" >상위퍼센트</button> </th>
                         `
+            } else {
+                console.log('false');
+                htmlTh += `
+                        <th> <button type="button" onclick="baseStatsAllPrint('아이디')">번호</button> </th>
+                        <th> 이미지</th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('한글이름')">포켓몬</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('타입')">타입</button> </th>
+                        <th> <button type="button" onclick="baseStatsAllPrint('${stats}')">${stats}</button> </th>
+                        <th> <button type="button" >상위퍼센트</button> </th>
+                        `
+            }
+
+            result.forEach(포켓몬 => {
+                if (stats == '총합') {
+                    htmlTb += `
+                            <tr>
+                                <td>${포켓몬.아이디}</td>
+                                <td><img src="${포켓몬.이미지}" /></td>
+                                <td>${포켓몬.한글이름} (${포켓몬.영어이름})</td>
+                                <td>${포켓몬.타입}</td>
+                                <td>${포켓몬.체력}</td>
+                                <td>${포켓몬.공격}</td>
+                                <td>${포켓몬.방어}</td>
+                                <td>${포켓몬.특수공격}</td>
+                                <td>${포켓몬.특수방어}</td>
+                                <td>${포켓몬.스피드}</td>
+                                <td>${포켓몬.총합}</td>
+                                <td>${포켓몬.상위퍼센트}%</td>
+                            </tr>
+                            `
+                } else {
+                    htmlTb += `
+                            <tr>
+                                <td>${포켓몬.아이디}</td>
+                                <td><img src="${포켓몬.이미지}" /></td>
+                                <td>${포켓몬.한글이름} (${포켓몬.영어이름})</td>
+                                <td>${포켓몬.타입}</td>
+                                <td>${포켓몬[stats]}</td>
+                                <td>${포켓몬.상위퍼센트}%</td>
+                            </tr>
+                            `
+                }
+
             })  // forEach end
-            tbody.innerHTML = html;
+            thead.innerHTML = htmlTh;
+            tbody.innerHTML = htmlTb;
         }   // success end
     })  // ajax end
 }   // baseStatsPercentPrint() end
