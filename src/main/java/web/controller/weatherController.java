e11e941f202d4a329a020ace126bb46329edb41b
wonsebe <wonsb03129@gmail.com>
2024-10-23 16:55:47 +0900

    원세빈 날씨 js 시간 보내는 중

---
+++ src/main/java/web/controller/weatherController.java
@@ -0,0 +1,14 @@
+package web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/weather")
+public class weatherController {
+
+    //날씨예측
+//    public
+}
