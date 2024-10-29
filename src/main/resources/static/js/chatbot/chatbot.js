9429619b091079418d65e3fdfe8d1152c9b375d6
wonsebe <wonsb03129@gmail.com>
2024-10-29 18:15:45 +0900

    1029 원세빈 커밋

--- src/main/resources/static/js/chatbot/chatbot.js
+++ src/main/resources/static/js/chatbot/chatbot.js
@@ -7,7 +7,7 @@ function roChat(){
 
     $.ajax({
         async:false,
-        url:'http://127.0.0.1:5000/chatbot/bot',
+        url:'http://127.0.0.1:5000/chatbot/seq2seq',
         method:'get',
         data:{
             text:text
@@ -18,7 +18,7 @@ function roChat(){
             let cPrint=document.querySelector('.cPrint')
             let html= cPrint.innerHTML;
             html += `<div class="userC">유저: ${text}</div><br/>`
-            html+=` <div class="botC">로토봇의 답변:${response}</div> <br/>`
+            html +=` <div class="botC">로토봇의 답변:${response}</div> <br/>`
             cPrint.innerHTML=html;
             
         },
